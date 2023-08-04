f6d19fcda0d49c7fca35acc96d35ddc6bfc0a5a5
a <tayrific357@Gmail.com>
2023-08-04 02:06:48 +0100

    LogIn styling

---
+++ src/profile/Profile.jsx
@@ -0,0 +1,11 @@
+import "./profile.scss"
+
+const Profile = () => {
+    return (
+        <div>
+            <h1 className="profile">Profile</h1>
+        </div>
+    );
+}
+
+export default Profile;
---
+++ src/register/Register.jsx
@@ -0,0 +1,11 @@
+import "./register.scss"
+
+const Register = () => {
+    return (
+        <div>
+            <h1 className="register">Register</h1>
+        </div>
+    );
+}
+
+export default Register;
